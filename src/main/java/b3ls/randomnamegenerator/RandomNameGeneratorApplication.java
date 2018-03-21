885685f8f1abfa87fca3908f21b6242a861fddc2
Ben Craig <bcraig83@gmail.com>
2018-03-21 21:49:03 +0000

    Initial commit.

---
+++ src/main/java/b3ls/randomnamegenerator/RandomNameGeneratorApplication.java
@@ -0,0 +1,12 @@
+package b3ls.randomnamegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNameGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNameGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/b3ls/randomnamegenerator/RandomNameGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package b3ls.randomnamegenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandomNameGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
