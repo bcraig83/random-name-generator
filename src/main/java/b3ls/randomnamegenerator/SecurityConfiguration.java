30c5e64d89fbaf953b60ec6b01953a6014ae0917
Ben Craig <bcraig83@gmail.com>
2018-03-21 22:32:05 +0000

    Start implementing spring security

---
+++ src/main/java/b3ls/randomnamegenerator/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package b3ls.randomnamegenerator;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("b3ls")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  // work-in-progress
+}
