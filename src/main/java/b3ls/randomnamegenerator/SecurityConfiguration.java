dfdddf02f1f4eddbd4b3bac9a44dddbb5a83b66f
Ben Craig <ben.craig@sita.aero>
2018-03-22 15:04:34 +0000

    Implement some basic security information.

--- src/main/java/b3ls/randomnamegenerator/SecurityConfiguration.java
+++ src/main/java/b3ls/randomnamegenerator/SecurityConfiguration.java
@@ -1,14 +1,20 @@
 package b3ls.randomnamegenerator;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-//@EnableWebSecurity
-@ComponentScan("b3ls")
-public class SecurityConfiguration /*extends WebSecurityConfigurerAdapter*/ {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  // work-in-progress
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER");
+  }
+
 }
