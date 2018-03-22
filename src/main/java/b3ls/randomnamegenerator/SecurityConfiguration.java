7bf69f3a6017f7edcbd95fde02f27ffd36579017
Ben Craig <ben.craig@sita.aero>
2018-03-22 14:59:04 +0000

    Disable security for now...

--- src/main/java/b3ls/randomnamegenerator/SecurityConfiguration.java
+++ src/main/java/b3ls/randomnamegenerator/SecurityConfiguration.java
@@ -2,13 +2,13 @@ package b3ls.randomnamegenerator;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 @ComponentScan("b3ls")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration /*extends WebSecurityConfigurerAdapter*/ {
 
   // work-in-progress
 }
